d417fc05417b7b149b45f10a5ccacbe33fe97194
DorianTs <doriants@gmail.com>
2016-03-22 19:38:45 +0200

    Added code that insures we won't get old stage back again in a new stage

--- JS/GameLogic.js
+++ JS/GameLogic.js
@@ -2,6 +2,29 @@
  * Created by DorianTs on 01/03/2016.
  */
 
+/*===================Defines=======================*/
+var LVL_EASY = 1;
+var LVL_MEDIUM = 2;
+var LVL_HARD = 3;
+
+ var MIN_EASY_STAGE = 1;
+ var MAX_EASY_STAGE = 5;
+ var MIN_MEDIUM_STAGE = 6;
+ var MAX_MEDIUM_STAGE = 10;
+ var MIN_HARD_STAGE = 11;
+ var MAX_HARD_STAGE = 15;
+ /*=================================================*/
+
+/*Declaring the global variables for the game*/
+var score = 0;
+var level = 1;
+var lives = 3; // how many times player can has mistakes
+var stage = 1;
+var clues = 3;
+var logoNumber;
+var logosDone = [];
+/*===================================================*/
+
 /*Logo is an object represents a logo with a slogan, its level(1-easy
 * 2-medium, 3-hard) and a link to a picture of the logo(will be used
 * for the help button)*/
@@ -31,45 +54,45 @@ function LogoList(){
 var logoLists = (function(){
     //starting with the easy part
     easy = new LogoList();
-    easy.addLogo("אסם", "זה טוב זה טוב זה", 1, "css/logos/easy/osem.jpg");
-    easy.addLogo("בזק", "הכי טוב בבית", 1, "css/logos/easy/bezeq.png");
-    easy.addLogo("012 smile", "סתם כי בא לי לדבר", 1, "css/logos/easy/012.png");
-    easy.addLogo("לאומי קארד", "תמיד, לפנק, לפנק, לפנק", 1, "css/logos/easy/leumi.jpg");
-    easy.addLogo("הראל", "אולי יש לכם ביטוח בריאות אבל אין לכם " , 1, "css/logos/easy/harel.png");
-    easy.addLogo("אלפא רומיאו", "זה שאתה נושם, לא אומר שאתה חי", 1, "css/logos/easy/romeo.jpg");
-    easy.addLogo("נביעות", "לגוף ולנשמה", 1, "css/logos/easy/neviot.jpg");
-    easy.addLogo("Nike", "just do it", 1, "css/logos/easy/nike.png");
-    easy.addLogo("תפוצ'יפס", "טבעי שלא תשלוט בעצמך", 1, "css/logos/easy/tapuchips.png");
-    easy.addLogo("אל על", "הכי בבית בעולם", 1, "css/logos/easy/elal.jpg");
-    easy.addLogo("מחסני תאורה", "באתי, נדלקתי!", 1, "css/logos/easy/teura.jpg");
-    easy.addLogo("צבר", "חומוס עושים באהבה או שלא עושים בכלל", 1, "css/logos/easy/tzabar.jpg");
+    easy.addLogo("אסם", "זה טוב זה טוב זה", LVL_EASY, "css/logos/easy/osem.jpg");
+    easy.addLogo("בזק", "הכי טוב בבית", LVL_EASY, "css/logos/easy/bezeq.png");
+    easy.addLogo("012 smile", "סתם כי בא לי לדבר", LVL_EASY, "css/logos/easy/012.png");
+    easy.addLogo("לאומי קארד", "תמיד, לפנק, לפנק, לפנק", LVL_EASY, "css/logos/easy/leumi.jpg");
+    easy.addLogo("הראל", "אולי יש לכם ביטוח בריאות אבל אין לכם " , LVL_EASY, "css/logos/easy/harel.png");
+    easy.addLogo("אלפא רומיאו", "זה שאתה נושם, לא אומר שאתה חי", LVL_EASY, "css/logos/easy/romeo.jpg");
+    easy.addLogo("נביעות", "לגוף ולנשמה", LVL_EASY, "css/logos/easy/neviot.jpg");
+    easy.addLogo("Nike", "just do it", LVL_EASY, "css/logos/easy/nike.png");
+    easy.addLogo("תפוצ'יפס", "טבעי שלא תשלוט בעצמך", LVL_EASY, "css/logos/easy/tapuchips.png");
+    easy.addLogo("אל על", "הכי בבית בעולם", LVL_EASY, "css/logos/easy/elal.jpg");
+    easy.addLogo("מחסני תאורה", "באתי, נדלקתי!", LVL_EASY, "css/logos/easy/teura.jpg");
+    easy.addLogo("צבר", "חומוס עושים באהבה או שלא עושים בכלל", LVL_EASY, "css/logos/easy/tzabar.jpg");
 
     medium = new LogoList();
-    medium.addLogo("Wobi", "משווים וקונים ביטוח", 2, "css/logos/medium/wobi.jpg");
-    medium.addLogo("רכבת ישראל", "פשוט ליהנות מהדרך", 2, "css/logos/medium/rakevet.jpg");
-    medium.addLogo("תנובה", "לגדול בבית ישראלי", 2, "css/logos/medium/tnuva.png");
-    medium.addLogo("Nescafe", "it all starts with a-", 2, "css/logos/medium/Nescafe.jpg");
-    medium.addLogo("מטרנה", "הכי קרוב לאמא", 2, "css/logos/medium/materna.png");
-    medium.addLogo("Renault", "Drive the change", 2, "css/logos/medium/reno.jpg");
-    medium.addLogo("קוקה קולה", "לאהוב את החיים", 2, "css/logos/medium/cola.png");
-    medium.addLogo("Apple", "Think different", 2, "css/logos/medium/apple.png");
-    medium.addLogo("H&O", "זה רק נראה יקר", 2, "css/logos/medium/hno.png");
-    medium.addLogo("McDonald's", "I'm Loving It", 2, "css/logos/medium/mcdonalds.png");
-    medium.addLogo("ניקול", "במטבח זה הכל", 2, "css/logos/medium/nikol.png");
-    medium.addLogo("הופ", "לגדול בידיים טובות", 2, "css/logos/medium/hop.png");
-    medium.addLogo("סימילאק", "בגלל המדע", 2, "css/logos/medium/similak.jpg");
+    medium.addLogo("Wobi", "משווים וקונים ביטוח", LVL_MEDIUM, "css/logos/medium/wobi.jpg");
+    medium.addLogo("רכבת ישראל", "פשוט ליהנות מהדרך", LVL_MEDIUM, "css/logos/medium/rakevet.jpg");
+    medium.addLogo("תנובה", "לגדול בבית ישראלי", LVL_MEDIUM, "css/logos/medium/tnuva.png");
+    medium.addLogo("Nescafe", "it all starts with a-", LVL_MEDIUM, "css/logos/medium/Nescafe.jpg");
+    medium.addLogo("מטרנה", "הכי קרוב לאמא", LVL_MEDIUM, "css/logos/medium/materna.png");
+    medium.addLogo("Renault", "Drive the change", LVL_MEDIUM, "css/logos/medium/reno.jpg");
+    medium.addLogo("קוקה קולה", "לאהוב את החיים", LVL_MEDIUM, "css/logos/medium/cola.png");
+    medium.addLogo("Apple", "Think different", LVL_MEDIUM, "css/logos/medium/apple.png");
+    medium.addLogo("H&O", "זה רק נראה יקר", LVL_MEDIUM, "css/logos/medium/hno.png");
+    medium.addLogo("McDonald's", "I'm Loving It", LVL_MEDIUM, "css/logos/medium/mcdonalds.png");
+    medium.addLogo("ניקול", "במטבח זה הכל", LVL_MEDIUM, "css/logos/medium/nikol.png");
+    medium.addLogo("הופ", "לגדול בידיים טובות", LVL_MEDIUM, "css/logos/medium/hop.png");
+    medium.addLogo("סימילאק", "בגלל המדע", LVL_MEDIUM, "css/logos/medium/similak.jpg");
 
     hard = new LogoList();
-    hard.addLogo("שופרסל", "לקנות כמו שצריך", 3, "css/logos/hard/shufersal.png");
-    hard.addLogo("דואר ישראל", "בכל מקום בשביל כולם", 3, "css/logos/hard/doar.png");
-    hard.addLogo("Mercedes", "The best or nothing", 3, "css/logos/hard/mercedes.png");
-    hard.addLogo("Subaru", "Confidence in motion", 3, "css/logos/hard/subaru.jpg");
-    hard.addLogo("Peugeot", "Motion & Emotion", 3, "css/logos/hard/pegu.png");
-    hard.addLogo("Kia", "The power to surprise", 3, "css/logos/hard/kia.png");
-    hard.addLogo("Seat", "Enjoyneering", 3, "css/logos/hard/seat.png");
-    hard.addLogo("BMW", "The Ultimate Driving Machine", 3, "css/logos/hard/bmw.jpg");
-    hard.addLogo("IBM", "Think", 3, "css/logos/hard/ibm.jpg");
-    hard.addLogo("L'oreal", "Because You're Worth It", 3, "css/logos/hard/loreal.jpg");
+    hard.addLogo("שופרסל", "לקנות כמו שצריך", LVL_HARD, "css/logos/hard/shufersal.png");
+    hard.addLogo("דואר ישראל", "בכל מקום בשביל כולם", LVL_HARD, "css/logos/hard/doar.png");
+    hard.addLogo("Mercedes", "The best or nothing", LVL_HARD, "css/logos/hard/mercedes.png");
+    hard.addLogo("Subaru", "Confidence in motion", LVL_HARD, "css/logos/hard/subaru.jpg");
+    hard.addLogo("Peugeot", "Motion & Emotion", LVL_HARD, "css/logos/hard/pegu.png");
+    hard.addLogo("Kia", "The power to surprise", LVL_HARD, "css/logos/hard/kia.png");
+    hard.addLogo("Seat", "Enjoyneering", LVL_HARD, "css/logos/hard/seat.png");
+    hard.addLogo("BMW", "The Ultimate Driving Machine", LVL_HARD, "css/logos/hard/bmw.jpg");
+    hard.addLogo("IBM", "Think", LVL_HARD, "css/logos/hard/ibm.jpg");
+    hard.addLogo("L'oreal", "Because You're Worth It", LVL_HARD, "css/logos/hard/loreal.jpg");
     hard.addLogo("MasterCard", "There are some things money can't buy. For everything else, there's-", 3, "css/logos/hard/mastercard.png");
 
     return {
@@ -79,14 +102,6 @@ var logoLists = (function(){
     };
 }() );
 
-
-//Declaring the global variables for the game
-var score = 0;
-var level = 1;
-var lives = 3; // how many times player can has mistakes
-var stage = 1;
-var clues = 3;
-var logoNumber;
 
 var clickListener = function(e) {
     if(e.target.id == "startgamebutt") {
@@ -104,6 +119,7 @@ var clickListener = function(e) {
 
         // add slogan of first logo on the screen
         logoNumber = Math.floor(Math.random() * (logoLists.easyList.list.length));
+        logosDone.push(logoNumber);
         $(".sloganPlace").html(logoLists.easyList.list[logoNumber].slogan);
         $("#answer").focus();
         $("#answer").attr('readonly',false);
@@ -211,16 +227,14 @@ var clickListener = function(e) {
             return;
         }
         $(".stage").html(stage);
-        logoNumber = getRandomLogoNumber();
+
+        // reset the 'stages done' array to the new level of game
+        if(stage == MIN_MEDIUM_STAGE || stage == MIN_HARD_STAGE)
+            logosDone = [];
+        setLogoNumber();
+        logosDone.push(logoNumber);
         $(".sloganPlace").html(getLogoSlogan());
-
-
-        /*Cleaning the screen for the next stage*/
-        $("#nextStage").hide();
-        $("#img").hide();
-        $("#answer").val('');
-        $("#answer").focus();
-        $("#answer").attr('readonly',false);
+        cleanScreen();
     }
 };
 
@@ -245,9 +259,9 @@ var loadPage = function(){
 /*Function checks what level the player in and return a string
 * that represents this level number*/
 function getLevel(){
-    if(level == 1)
+    if(level == LVL_EASY)
         return "Easy";
-    else if (level == 2)
+    else if (level == LVL_MEDIUM)
         return "Medium";
     else return "Hard";
 }
@@ -256,9 +270,9 @@ function getLevel(){
 * logoSymbol for that level*/
 function getLinkToLogo(){
     var link;
-    if(level == 1)
+    if(level == LVL_EASY)
         link = logoLists.easyList.list[logoNumber].logoSymbol;
-    else if(level == 2)
+    else if(level == LVL_MEDIUM)
         link = logoLists.mediumList.list[logoNumber].logoSymbol;
     else
         link = logoLists.hardList.list[logoNumber].logoSymbol;
@@ -269,9 +283,9 @@ function getLinkToLogo(){
  * name of logo for that level*/
 function getLogoName(){
     var name;
-    if(level == 1)
+    if(level == LVL_EASY)
         name = logoLists.easyList.list[logoNumber].name;
-    else if(level == 2)
+    else if(level == LVL_MEDIUM)
         name = logoLists.mediumList.list[logoNumber].name;
     else
         name = logoLists.hardList.list[logoNumber].name;
@@ -282,9 +296,9 @@ function getLogoName(){
  * slogan of logo for that level*/
 function getLogoSlogan(){
     var slogan;
-    if(level == 1)
+    if(level == LVL_EASY)
         slogan = logoLists.easyList.list[logoNumber].slogan;
-    else if(level == 2)
+    else if(level == LVL_MEDIUM)
         slogan = logoLists.mediumList.list[logoNumber].slogan;
     else slogan = logoLists.hardList.list[logoNumber].slogan;
     return slogan;
@@ -293,9 +307,9 @@ function getLogoSlogan(){
 /*Function returns the current level*/
 function getLogoLevel(){
     var lv;
-    if(level == 1)
+    if(level == LVL_EASY)
         lv = logoLists.easyList.list[logoNumber].level;
-    else if(level == 2)
+    else if(level == LVL_MEDIUM)
         lv = logoLists.mediumList.list[logoNumber].level;
     else
         lv = logoLists.hardList.list[logoNumber].level;
@@ -305,9 +319,9 @@ function getLogoLevel(){
 /*Function gets the logo level the player succeeded to guess
 * right and update his new score according to the level*/
 function updateScore(logoLevel){
-    if(logoLevel == 1)
+    if(logoLevel == LVL_EASY)
         score += 10;
-    else if(logoLevel == 2)
+    else if(logoLevel == LVL_MEDIUM)
         score += 25;
     else score += 50;
 }
@@ -315,22 +329,22 @@ function updateScore(logoLevel){
 /*Function updates the current level by the current stage
 * the player in*/
 function updateLevel(){
-    if(stage >=1 && stage <=5)
+    if(stage >=MIN_EASY_STAGE && stage <=MAX_EASY_STAGE)
     {
-        level = 1;
+        level = LVL_EASY;
         $(".level").html(getLevel());
         return;
     }
-    if(stage >=6 && stage <=10)
+    if(stage >=MIN_MEDIUM_STAGE && stage <=MAX_MEDIUM_STAGE)
     {
-        level = 2;
+        level = LVL_MEDIUM;
         $(".level").html(getLevel());
         return;
     }
 
-    else if(stage>=11 && stage<=15)
+    else if(stage>=MIN_HARD_STAGE && stage<=MAX_HARD_STAGE)
     {
-        level = 3;
+        level = LVL_HARD;
         $(".level").html(getLevel());
         return;
     }
@@ -343,12 +357,36 @@ function updateLevel(){
 * and return it*/
 function getRandomLogoNumber(){
     var num;
-    if(level == 1)
+    if(level == LVL_EASY)
         num = Math.floor(Math.random() * (logoLists.easyList.list.length));
-    else if(level == 2)
+    else if(level == LVL_MEDIUM)
         num = Math.floor(Math.random() * (logoLists.mediumList.list.length));
     else num = Math.floor(Math.random() * (logoLists.hardList.list.length));
     return num;
 }
 
+/*Function set the logo number player is about to play.
+* function makes sure level wasn't played before*/
+function setLogoNumber(){
+    logoNumber = getRandomLogoNumber();
+    var valid = false;
+    var i;
+    while(!valid) {
+        for (i = 0; i < logosDone.length; i++) {
+            if (logoNumber == logosDone[i]) {
+                logoNumber = getRandomLogoNumber();
+                break;
+            }
+        }
+        valid = true;
+    }
+}
 
+/*Function cleans the screen for the next stage*/
+function cleanScreen() {
+    $("#nextStage").hide();
+    $("#img").hide();
+    $("#answer").val('');
+    $("#answer").focus();
+    $("#answer").attr('readonly', false);
+}
